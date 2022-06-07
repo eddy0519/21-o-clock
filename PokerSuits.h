aa499dc9ea41e4714ffda80b9ab2355809058bc0
eddy0519 <79899855+eddy0519@users.noreply.github.com>
2022-06-07 12:14:59 +0800

    Create PokerSuits.h

---
+++ PokerSuits.h
@@ -0,0 +1,15 @@
+#ifndef pokerSuits_h
+#define pokerSuits_h
+#include <string>
+
+using namespace std;
+
+class PokerSuits
+{
+public:
+	PokerSuits();
+	string getPokerSuits(int var);
+private:
+	string pokerSuits[52]; //¼³§Jªá¦â
+};
+#endif
